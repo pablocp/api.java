73dd50df31c7c979fb50f35943399ff79c88ae3e
Igor Spasić <igor.spasic@liferay.com>
2015-07-10 18:05:22 +0200

    Fixed test and added new one.

--- api/src/test/java/com/liferay/launchpad/api/GoogleTest.java
+++ api/src/test/java/com/liferay/launchpad/api/GoogleTest.java
@@ -1,5 +1,6 @@
 package com.liferay.launchpad.api;
 
+import com.liferay.launchpad.sdk.Response;
 import org.junit.Assert;
 import org.junit.Test;
 public class GoogleTest {
@@ -19,17 +20,44 @@ public class GoogleTest {
 
 	@Test
 	public void testGoogleHomePage_invalid() {
+		final Response[] response = new Response[1];
+		final Throwable[] throwable = new Throwable[1];
+
 		LaunchpadClient
 			.url("http://google.com/404")
 			.get()
 			.whenComplete((clientResponse, e) -> {
-				Assert.assertEquals(404, clientResponse.statusCode());
-				Assert.assertTrue(
-					e.getCause() instanceof LaunchpadClientException);
+				response[0] = clientResponse;
+				throwable[0] = e.getCause();
 			})
 			.thenAccept((clientResponse) -> Assert.fail("Must complete exceptionally 404"))
 			.exceptionally(e -> null)
 			.join();
+
+		Assert.assertNull(response[0]);
+		Assert.assertTrue(throwable[0] instanceof ResponseErrorException);
+	}
+
+	@Test
+	public void testGoogleHomePage_invalid_noException() {
+		final Response[] response = new Response[1];
+		final Throwable[] throwable = new Throwable[1];
+
+		LaunchpadClient
+			.url("http://google.com/404")
+			.throwExceptionOnResponseError(false)
+			.get()
+			.whenComplete((clientResponse, e) -> {
+				response[0] = clientResponse;
+				throwable[0] = e.getCause();
+			})
+			.thenAccept((clientResponse) -> Assert.fail("Must complete exceptionally 404"))
+			.exceptionally(e -> null)
+			.join();
+
+		Assert.assertNotNull(response[0]);
+		Assert.assertNull(throwable[0]);
+		Assert.assertEquals(404, response[0].statusCode());
 	}
 
 }
