a103d252d908b80465bd4c3accba86318fafea89
Igor Spasić <igor.spasic@liferay.com>
2015-07-15 00:56:24 +0300

    Added one more CF example

--- api/src/test/java/com/liferay/launchpad/api/CompletableFutureTest.java
+++ api/src/test/java/com/liferay/launchpad/api/CompletableFutureTest.java
@@ -56,6 +56,10 @@ public class CompletableFutureTest {
 		}
 	}
 
+	/**
+	 * Example how exception in the first thenApply block stops the execution
+	 * of the completable future, so second thenApply is not executed.
+	 */
 	@Test
 	public void testSupply_thenApplyException_thenApply() {
 		final MutableInteger access = new MutableInteger(0);
@@ -91,6 +95,47 @@ public class CompletableFutureTest {
 		catch (Exception e) {
 			fail();
 		}
+	}
+
+	/**
+	 * When exceptionally catch the throwable and does not return it,
+	 * the flow continues. The same happens with the .handle().
+	 */
+	@Test
+	public void testSupply_thenApplyException_withExceptionally() {
+		final MutableInteger access = new MutableInteger(0);
+
+		CompletableFuture<Integer> f =
+			CompletableFuture.supplyAsync(() -> {
+				ThreadUtil.sleep(500);
+				access.value++;
+				return new MutableInteger(1);
+			})
+			.thenApply(new Function<MutableInteger, MutableInteger>() {
+				@Override
+				public MutableInteger apply(MutableInteger mi) {
+					throw new IllegalArgumentException();
+				}
+			})
+			.exceptionally(new Function<Throwable, MutableInteger>() {
+				@Override
+				public MutableInteger apply(Throwable throwable) {
+					if (throwable.getCause() instanceof IllegalArgumentException) {
+						return new MutableInteger(123);
+					}
+					throw new RuntimeException("failure");
+				}
+			})
+			.thenApply(mi -> {
+				access.value++;
+				return Integer.valueOf(mi.value);
+			});
+
+
+		Integer m = f.join();
+
+		assertEquals(2, access.value);
+		assertEquals(123, m.intValue());
 	}
 
 }
