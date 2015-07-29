0b4a19b5d88e6104ae17f24d9078334e374c898d
Igor Spasić <igor.spasic@liferay.com>
2015-07-29 16:42:11 +0200

    Removed api-json-jodd module

--- api-json-jodd/src/main/java/com/liferay/launchpad/api/LaunchpadClientJsonEngineBinder.java
+++
@@ -1,9 +0,0 @@
-package com.liferay.launchpad.api;
-
-public class LaunchpadClientJsonEngineBinder implements JsonEngineBinder {
-	@Override
-	public JsonEngine initJsonEngine() {
-		return new JoddJsonEngine();
-	}
-
-}
