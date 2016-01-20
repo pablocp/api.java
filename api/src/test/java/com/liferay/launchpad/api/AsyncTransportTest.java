40ab8d80a58307b61acc7b5a2161d4cd064f6ce4
Lais Andrade <lais.andrade@liferay.com>
2016-01-20 18:22:57 -0300

    Adds tests for com.liferay.launchpad.api package.

---
+++ api/src/test/java/com/liferay/launchpad/api/AsyncTransportTest.java
@@ -0,0 +1,88 @@
+package com.liferay.launchpad.api;
+
+import com.liferay.launchpad.ApiClient;
+import com.liferay.launchpad.sdk.Request;
+import com.liferay.launchpad.sdk.Response;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+public class AsyncTransportTest {
+
+	@BeforeClass
+	public static void beforeClass() {
+		ApiClient.init();
+	}
+
+	@Test
+	public void testSend_withException() {
+		try {
+			Launchpad
+				.url("http://google.com")
+				.use(new AsyncTransport() {
+					@Override
+					public CompletableFuture<Response> sendAsync(
+						Request request) {
+
+						return CompletableFuture.supplyAsync(() -> {
+							throw new Error("Error");
+						});
+					}
+
+				})
+				.get();
+
+			Assert.fail("No exception thrown.");
+		}
+		catch (LaunchpadClientException e) {
+			Assert.assertEquals("Transport failed", e.getMessage());
+			Assert.assertTrue(e.getCause() instanceof Error);
+		}
+	}
+
+	@Test
+	public void testSend_withRuntimeException() {
+		try {
+			Launchpad
+				.url("http://google.com")
+				.use(new AsyncTransport() {
+					@Override
+					public CompletableFuture<Response> sendAsync(
+						Request request) {
+
+						return CompletableFuture.supplyAsync(() -> {
+							throw new RuntimeException("Error");
+						});
+					}
+
+				})
+				.get();
+
+			Assert.fail("No exception thrown.");
+		}
+		catch (RuntimeException e) {
+			Assert.assertEquals("Error", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testSend_withSuccessfulRequest() {
+		Response clientResponse = Launchpad
+			.url("http://google.com")
+			.use(new AsyncTransport() {
+				@Override
+				public CompletableFuture<Response> sendAsync(Request request) {
+					return CompletableFuture.completedFuture(
+						new TestTransport().send(request));
+				}
+
+			})
+			.get();
+
+		Assert.assertNotNull(clientResponse);
+		Assert.assertTrue(clientResponse.succeeded());
+	}
+
+}
